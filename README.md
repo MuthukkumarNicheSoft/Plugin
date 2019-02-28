3cdcd33e6dfc4e4fcbff22e1e0c002fd9c17a5fa
Muthukkumar <muthukkumar@nichesoft.net>
2019-02-28 12:45:53 +0530

    added the images and modified the plugin.rb

---
+++ README.md
@@ -0,0 +1,3 @@
+# discourse-theme-creator
+
+Allows users to create, edit and share their own themes on theme-creator.discourse.org. This plugin is not designed to be installed on normal discourse instances.
--- plugin.rb
+++ plugin.rb
@@ -2,4 +2,6 @@
 # about: A super simple plugin to demonstrate how plugins work
 # version: 0.0.1
 # authors: Awesome Plugin Developer
+
 enabled_site_setting :awesomeness_enabled
+register_asset "stylesheets/common.scss"
